95d2bd2d9b36a1fd5fb0df356a7c7c6ece733d41
Catrine.Bergstromner <catrine.bergstromner@additude.se>
2021-03-06 11:33:07 +0100

    Added BasicCalculator, BasicOperations and Main classes

---
+++ src/main/java/com/nackademin/catrine_bergstromner_assignment02/BasicCalculator.java
@@ -0,0 +1,10 @@
+package com.nackademin.catrine_bergstromner_assignment02;
+
+public class BasicCalculator implements BasicOperations {
+
+	@Override
+	public double addition(double num1, double num2) {
+		return num1 + num2;
+	}
+
+}
---
+++ src/main/java/com/nackademin/catrine_bergstromner_assignment02/BasicOperations.java
@@ -0,0 +1,6 @@
+package com.nackademin.catrine_bergstromner_assignment02;
+
+public interface BasicOperations {
+	public double addition (double num1, double num2);
+
+}
